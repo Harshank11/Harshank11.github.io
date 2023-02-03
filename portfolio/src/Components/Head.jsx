75deae4cea8c56c6a4612d2e656e38081a6268aa
Harshank11 <hrsk321@gmail.com>
2023-02-03 16:27:45 +0530

    added animation to title

--- portfolio/src/Components/Head.jsx
+++ portfolio/src/Components/Head.jsx
@@ -1,6 +1,6 @@
 import { Button, Center, Heading, Text, Flex, Image,Box } from "@chakra-ui/react";
 import React from "react";
-// import "./Head.css";
+import "./Head.css";
 
 export function Head() {
  
@@ -8,10 +8,10 @@ export function Head() {
     <Center textColor={"white"} mt={['10%']} borderColor={"1px solid white"} padding={"3%"}>
       
         <Heading >
-          <Text w={"80%"} >Hello,</Text>
-          <Text w={"80%"}>I am, Harshank Patel </Text>
-          <Box w={"80%"}>Full Stack Web Developer</Box>
-          <Text w={"80%"}>
+          <Text w={"80%"} className='animatedetail'>Hello,</Text>
+          <Text w={"80%"} className='animatetitle'>I am, Harshank Patel </Text>
+          <Box w={"80%"} className='animatetitle' >Full Stack Web Developer</Box>
+          <Text w={"80%"} className='animatedetail'>
             Passionate about Frontend Web Development & problem solving. I love
             building unique and cool projects and work in a team. I believe in
             continuous learning and growth. I believe in skillset, toolset and
